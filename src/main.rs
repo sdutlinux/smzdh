035a02bb51dca3a7108c66951749c107906c16f2
paomian <qtang@leancloud.rocks>
2016-05-12 22:13:03 +0800

    (Feat) add lib

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,27 @@
+#![feature(custom_derive, plugin)]
+#![plugin(serde_macros,clippy)]
+
 extern crate iron;
 extern crate router;
+extern crate serde_json;
 
 use iron::prelude::*;
 use iron::status;
 use router::Router;
+use std::collections::BTreeMap;
+
+fn handler(req: &mut Request) -> IronResult<Response> {
+
+    println!("{}",serde_json::to_string(&BTreeMap::<String,String>::new()).unwrap());
+    let query = req.extensions.get::<Router>().unwrap().find("query").unwrap_or("/");
+    Ok(Response::with((status::Ok, query)))
+}
 
 fn main() {
-    let mut router = Router::new();  // Alternative syntax:
-    router.get("/", handler);        // let router = router!(get "/" => handler,
-    router.get("/:query", handler);  //                      get "/:query" => handler);
+    let mut router = Router::new();
+    router.get("/", handler);
+    router.get("/:query", handler);
+    router.post("/hello", handler);
 
-    Iron::new(router).http("localhost:3000").unwrap();
-
-    fn handler(req: &mut Request) -> IronResult<Response> {
-    let ref query = req.extensions.get::<Router>().unwrap().find("query").unwrap_or("/");
-    Ok(Response::with((status::Ok, *query)))
-}
+    let _ = Iron::new(router).http("localhost:3000");
 }
