b3b591afd11c031ebad5f7fcf6c6f15ffe778500
paomian <qtang@leancloud.rocks>
2016-05-12 13:41:17 +0800

    (Chore) add lib

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
+extern crate iron;
+extern crate router;
+
+use iron::prelude::*;
+use iron::status;
+use router::Router;
+
 fn main() {
-    println!("Hello, world!");
+    let mut router = Router::new();  // Alternative syntax:
+    router.get("/", handler);        // let router = router!(get "/" => handler,
+    router.get("/:query", handler);  //                      get "/:query" => handler);
+
+    Iron::new(router).http("localhost:3000").unwrap();
+
+    fn handler(req: &mut Request) -> IronResult<Response> {
+    let ref query = req.extensions.get::<Router>().unwrap().find("query").unwrap_or("/");
+    Ok(Response::with((status::Ok, *query)))
+}
 }
