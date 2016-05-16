25d1896eb5bc8030dc1ae41057d724662bdeb1fb
paomian <xpaomian@gmail.com>
2016-05-16 23:39:38 +0800

    (Feat) add database and handler mod

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,48 @@
 #![feature(custom_derive, plugin)]
 #![plugin(serde_macros,clippy)]
 
+#[macro_use]
+extern crate log;
+#[macro_use]
+extern crate lazy_static;
+
+extern crate log4rs;
 extern crate iron;
 extern crate router;
 extern crate serde_json;
+extern crate postgres;
+extern crate redis;
+
+
+mod database;
+mod handlers;
 
 use iron::prelude::*;
 use iron::status;
 use router::Router;
-use std::collections::BTreeMap;
+//use std::collections::BTreeMap;
 
 fn handler(req: &mut Request) -> IronResult<Response> {
-
-    println!("{}",serde_json::to_string(&BTreeMap::<String,String>::new()).unwrap());
+    info!("Some thing {:?}",req.extensions.len());
+    //println!("{}",serde_json::to_string(&BTreeMap::<String,String>::new()).unwrap());
     let query = req.extensions.get::<Router>().unwrap().find("query").unwrap_or("/");
     Ok(Response::with((status::Ok, query)))
 }
 
+fn test(_: &mut Request) -> IronResult<Response> {
+    database::test::test();
+    Ok(Response::with((status::Ok, "test")))
+}
+
 fn main() {
+    match log4rs::init_file("config/log4rs.yaml", Default::default()) {
+        Ok(_) => info!("Log4rs start success"),
+        Err(e) => println!("{:?}",e),
+    }
     let mut router = Router::new();
     router.get("/", handler);
-    router.get("/:query", handler);
-    router.post("/hello", handler);
+    //router.get("/:query", handler);
+    router.get("/hello", test);
 
     let _ = Iron::new(router).http("localhost:3000");
 }
